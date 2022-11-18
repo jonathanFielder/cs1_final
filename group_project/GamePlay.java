7df1eeec28983948e3d7eaa422713b2ee21eb445
Jonathan Fielder <50757815+jonathanFielder@users.noreply.github.com>
2022-11-18 09:23:30 -0500

    first commit with skeleton code
    Her is the skeleton code to get started

---
+++ group_project/GamePlay.java
@@ -0,0 +1,94 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+/**
+ * The Game class.
+ *
+ * @author (your name here)
+ * @version (version or date)
+ */
+public class GamePlay
+{
+    //Put constants here
+
+    //Put instance variables here
+
+    /**
+     * No-arg GamePlay Constructor.
+     *
+     */
+    public GamePlay() throws FileNotFoundException
+    {
+        
+    }
+
+    /**
+     * Two-arg GamePlay Constructor.
+     *
+     * @param filename filename of terms and definitions to read
+     * @param selection menu option of user for game choice
+     */
+    public GamePlay(String filename)
+    {
+        
+    }
+
+    /**
+     * Method makeSelection.
+     *
+     * @param selection menu choice of user
+     */
+    public void makeSelection(int selection)
+    {
+       
+    }
+    
+    /**
+     * Method playRound.
+     *
+     */
+    public void playRound()
+    {
+        
+    }
+    
+    /**
+     * Method playLimitedGame.
+     *
+     * @param rounds number of rounds to play
+     */
+    public void playLimitedGame(int rounds)
+    {
+        
+    }
+    
+    /**
+     * Method playContinuousGame.
+     *
+     */
+    public void playContinuousGame()
+    {
+        
+    }
+    
+    /**
+     * Method displayRound.
+     *
+     * @param entries entries to build question
+     * @param index index of the term to find the correct definition
+     */
+    public void displayRound(Entry[] entries, int index)
+    {
+        
+    }
+    
+    /**
+     * Method printResults.
+     *
+     */
+    public void printResults()
+    {
+       
+    }
+}
---
+++ group_project/Glossary.java
@@ -0,0 +1,93 @@
+/**
+//Put required imports here
+
+/**
+ * The Glossary class.
+ *
+ * @author (your name here)
+ * @version (version or date)
+ */
+public class Glossary
+{
+    //Put constants here
+
+    //Put instance variables here
+
+    /**
+     * No-arg constructor for objects of class Glossary.
+     */
+    public Glossary()
+    {
+
+    }
+
+    /**
+     * N-arg constructor for objects of class Glossary.
+     * 
+     * @param initialSize size of entry array for glossary
+     */
+    public Glossary(int initialSize)
+    {
+
+    }
+
+    /**
+     * Method getNumEntries.
+     *
+     * @return The number of enries in glossary
+     */
+    public int getNumEntries()
+    {
+        return -1;
+    }
+
+    /**
+     * Method getEntries.
+     *
+     * @return The entries in the glossary
+     */
+    public Entry[] getEntries()
+    {
+        return new Entry[0];
+    }
+
+    /**
+     * Method readFile.
+     *
+     * @param filename name of file to read
+     */
+    public void readFile(String filename) 
+    {
+
+    }
+
+    /**
+     * Method addEntry.
+     *
+     * @param entry to be added
+     */
+    public void addEntry(Entry entry)
+    {
+
+    }
+
+    /**
+     * Method removeEntry.
+     *
+     * @param entry to be added
+     */
+    public void removeEntry(String term)
+    {
+
+    }
+
+    /**
+     * Method toString.
+     *
+     * @return The string representation of the glossary
+     */
+    public String toString()
+    {
+        return "";
+    }
+}
