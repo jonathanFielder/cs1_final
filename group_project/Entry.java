2098b50f20f82757ba4e44350662ce8c4d80562a
Erik Page <111699638+ErikEPage@users.noreply.github.com>
2022-11-19 15:02:11 -0500

    Add files via upload

--- group_project/Entry.java
+++ group_project/Entry.java
@@ -17,66 +17,103 @@
  * @version 19/11/22
  */
 public class Entry
-{
-    //Put instance variables below this line.  
+{  
     private String term;
     private String definition;
     
     /**
-     * No parameter constructor for objects of class Entry.
+     * No-arg constructor for objects of class Entry.
      */
     public Entry()
     {
-        
+    
     }
     
+    /**
+     * One-arg constructor for objects of class Entry.
+     */
     public Entry(String term)
     {
         this.term = term;
     }
     
+    /**
+     * Two-arg constructor for objects of class Entry.
+     */    
     public Entry(String term, String definition)
     {
         this.term = term;
         this.definition = definition;
     }
     
+    //set methods
+    /**
+     * Method setTerm.
+     *
+     * @param term entry term
+     */    
     public void setTerm(String term)
     {
         this.term = term;
     }
     
+    /**
+     * Method setDefinition.
+     *
+     * @param definition entry definition
+     */    
     public void setDefinition(String definition)
     {
         this.definition = definition;
     }
     
+    //get methods
+    /**
+     * Method getTerm.
+     *
+     * @return The entry term
+     */    
     public String getTerm()
     {
         return term;
     }
     
+    /**
+     * Method getDefinition.
+     *
+     * @return The entry definition
+     */    
     public String getDefinition()
     {
         return definition;
     }
     
-    
+    /**
+     * Method toString.
+     *
+     * @return The string representation of entry
+     */    
     public String toString()
     {
         return term + " - " + definition + ".\n";
     }
     
-    public boolean equals(Entry obj)
+    /**
+     * Method equals.
+     *
+     * @param entry entry to compare to
+     * @return true if this entry and param entry are the same
+     */    
+    public boolean equals(Entry entry)
     {
-        if (obj == null)
+        if (entry == null)
         {
             return false;
         }
-        if (obj == this)
+        if (entry == this)
         {
             return true;
         }
-        return this.getDefinition().equals(obj.getDefinition());
+        return this.getDefinition().equals(entry.getDefinition());
     }
 }
