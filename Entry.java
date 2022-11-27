ca0c05c698a9f7816c243dfbeeeebb6db392741e
Sean Choi <72466770+schoi88@users.noreply.github.com>
2022-11-26 21:17:17 -0500

    Add files via upload

---
+++ Entry.java
@@ -0,0 +1,93 @@
+/**
+ * The Entry class.
+ * 
+ * @author (your name here)
+ * @version (version or date)
+ */
+public class Entry
+{
+    /**
+     * No-arg constructor for objects of class Entry.
+     */
+    public Entry()
+    {
+        
+    }
+    
+    /**
+     * One-arg constructor for objects of class Entry.
+     */
+    public Entry(String term)
+    {
+              
+    }
+    
+    /**
+     * Two-arg constructor for objects of class Entry.
+     */
+    public Entry(String term, String definition)
+    {
+        
+    }
+    
+    /**
+     * Method setTerm.
+     *
+     * @param term entry term
+     */
+    public void setTerm(String term)
+    {
+       
+    }
+    
+    /**
+     * Method setDefinition.
+     *
+     * @param definition entry definition
+     */
+    public void setDefinition(String definition)
+    {
+        
+    }
+    
+    /**
+     * Method getTerm.
+     *
+     * @return The entry term
+     */
+    public String getTerm()
+    {
+       return "";
+    }
+    
+    /**
+     * Method getDefinition.
+     *
+     * @return The entry definition
+     */
+    public String getDefinition()
+    {
+       return ""; 
+    }
+
+    /**
+     * Method toString.
+     *
+     * @return The string representation of entry
+     */
+    public String toString()
+    {
+        return "";
+    }
+    
+    /**
+     * Method equals.
+     *
+     * @param entry entry to compare to
+     * @return true if this entry and param entry are the same
+     */
+    public boolean equals(Entry entry)
+    {
+       return false;
+    }
+}
